82a2474cf2ce3f744b767d4494051c0291c3c5aa
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-22 14:43:28 -0700

    updated

--- _strinlen.c
+++ _strinlen.c
@@ -5,13 +5,14 @@
  * @strinP: Collects a string pointer
  * Return: The number of string printed
  */
-int _strinlen(char *strinP)
+int _strinlen(char *s)
 {
 	int p = 0, number = 0;
 
-	for (p; strinP[p] != 0; p++)
+	while (s[p] != 0)
 	{
 		number++;
+		p++;
 	}
 	return (number);
 }
