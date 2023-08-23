605dbc54e0518ee4692f42a8ea7cfd07ee499412
MissPhumy <missphumy@gmail.com>
2023-08-23 07:26:23 -0700

    printf update

--- printf.c
+++ printf.c
@@ -43,7 +43,7 @@ int _printf(const char *format, ...)
 			strin = va_arg(lis, char *);
 				if (strin == NULL)
 					strin = "(null)";
-				dist = strinlen(strin);
+				dist = strlen(strin); /*corrected function name from ' strinlen ' to ' strlen '*/
 				write(1, strin, dist);
 				counter += dist;
 			}
