5d138606ebd72f1af98ba055e8d8efd5f09c8f66
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-25 13:26:26 -0700

    int_Handle.c

---
+++ int_handle.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdarg.h>
+#include "main.h"
+
+/**
+ * int_handle - Prints formatted output to stdout
+ * @format: The format string
+ * @..: Variable nuber of arguments.
+ * Return: The number of charecters printed 
+ */
+int int_Handle(const char *format, ..);
+int int_Handle(const char *format, ..)
+{
+	va_list pos;
+	
+	int b, neg, count = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(pos, format);
+	for (b=0; format && format[b] != '\0'; b++)
+	{
+		if (format[b] == 'd' || format[b] == 'i')
+		{
+			neg = va_arg(pos, int);
+			count += print_number(neg);
+		}
+	}
+	va_end(pos);
+	return (count);
+}
--- print_number.c
+++ print_number.c
@@ -5,6 +5,8 @@
  */
 void print_number(int n)
 {
+	char digit;
+
 	if (n < 0)
 	{
 		write(1, "-", 1);
@@ -14,7 +16,7 @@ void print_number(int n)
 	if (n / 10)
 		print_number(n / 10);
 
-	char digit = '0' + n % 10;
+	digit = '0' + n % 10;
 
 	write(1, &digit, 1);
 
